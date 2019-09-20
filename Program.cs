bcce3c5eb2c33518a642a5284dedabe48c73e7e9
Lian <liana.khachatryan1999@mail.ru>
2019-09-20 19:11:04 +0400

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StarRectangular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Base;
+
+            Console.WriteLine("========================= Star Rectangular =========================\n");
+
+
+             Console.Write("Enter Base: ");
+            Base = Int32.Parse(Console.ReadLine());
+        
+            for(int i=1;i<=Base;i+=2)
+            {
+                  for(int k=0;k<(Base-i)/2;k++)
+                  {
+                     Console.Write(" "); 
+                  }
+                for(int j=0;j<i;j++)
+                {         
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                
+                   
+            
+            }
+        }
+
+
+
+
+            
+    }
+}
